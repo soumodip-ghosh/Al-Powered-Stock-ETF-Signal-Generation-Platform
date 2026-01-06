2e3cd59434d4cfa17bb00b407903ca1b70c0e795
soumodip-ghosh <soumodipghoshofficial@gmail.com>
2026-01-06 12:56:44 +0530

    error Correction

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.post("http://127.0.0.1:8000/run-pipeline")
-response.raise_for_status()
-
-# 🔥 Safe to read parquet now
-import pandas as pd
-df = pd.read_parquet("data/clean_market.parquet")
-print(df.head())
